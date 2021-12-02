41ff52e8846235e3d0652f9a421855e840e6f55a
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-02 14:03:36 -0800

    created slogan button event listener

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const castleImg = document.querySelector('#castle-image');
 const waterfrontImg = document.querySelector('#waterfront-image');
 const sloganInputElem = document.querySelector('#slogan-input');
 const sloganButton = document.querySelector('#slogan-button');
+const sloganListElem = document.querySelector('.list');
 const countElem = document.querySelector('.count');
 
 // let state
 let skylineCount = 0;
 let castleCount = 0;
 let waterfrontCount = 0;
-let sloganArray = 0;
+let sloganArray = [];
 
 // set event listeners 
 skylineDropdown.addEventListener('change', () => {
@@ -49,6 +50,26 @@ waterfrontDropdown.addEventListener('change', () => {
     countElem.textContent = `You changed the waterfront ${waterfrontCount} times`;
 });
 
+sloganButton.addEventListener('click', () => {
+    const newSlogan = sloganInputElem.value;
+// push slogan to array of slogans `array.push(slogan)`
+    sloganArray.push(newSlogan);
+
+    sloganListElem.textContent = '';
+// loop through slogan array
+    for (let slogan of sloganArray) {
+      //create p tag
+        const p = document.createElement('p');
+
+        p.classList.add('slogan');
+
+        p.textContent = slogan;
+      //append each slogan
+        sloganListElem.append(p);  
+    }
+
+    sloganInputElem.value = '';
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
